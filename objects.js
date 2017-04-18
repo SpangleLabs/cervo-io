a2b0e808710c9c77c84c3dea46cd9fdc7b8e550a
joshcoales <joshua@coales.co.uk>
2017-04-18 15:47:25 +0100

    Renaming object and making link to individual zoo page

--- objects.js
+++ objects.js
@@ -6,9 +6,15 @@ SpeciesObj.prototype.species_id = 1;
 SpeciesObj.prototype.name = "name";
 SpeciesObj.prototype.category_id = 1;
 
-function Category(){}
-Category.prototype.category_id = 1;
-Category.prototype.name = "name";
-Category.prototype.parent_category_id = 1;
-Category.prototype.sub_categories = [Category];
-Category.prototype.species = [SpeciesObj];
+function CategoryObj(){}
+CategoryObj.prototype.category_id = 1;
+CategoryObj.prototype.name = "name";
+CategoryObj.prototype.parent_category_id = 1;
+CategoryObj.prototype.sub_categories = [CategoryObj];
+CategoryObj.prototype.species = [SpeciesObj];
+
+function ZooObj(){}
+ZooObj.prototype.zoo_id = 1;
+ZooObj.prototype.name = "name";
+ZooObj.prototype.postcode = "postcode";
+ZooObj.prototype.link = "link";
