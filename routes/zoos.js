cdc0f84376bc9db34a44c239e2a14afbc2857c4a
joshcoales <joshua@coales.co.uk>
2017-04-18 20:40:38 +0100

    Adding zoo_species to zoos/id output

--- routes/zoos.js
+++ routes/zoos.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var Zoos = require("../models/Zoos.js");
+var ZooSpecies = require("../models/ZooSpecies.js");
+var Promise = require("promise");
 var router = express.Router();
 
 /* GET zoos listing. */
@@ -8,7 +10,26 @@ router.get('/:id?', function(req, res, next) {
         Zoos.getZooById(req.params.id).catch(function(err) {
             res.json(err);
         }).then(function(rows) {
-            res.json(rows);
+            var zooSpecies = [];
+            for(var a = 0; a < rows.length; a++) {
+                zooSpecies.push(ZooSpecies.getZooSpeciesByZooId(rows[a].zoo_id));
+            }
+            Promise.all(zooSpecies).catch(function(err) {
+                res.json(err);
+            }).then(function(values) {
+                var data = [];
+                for(var b = 0; b < rows.length; b++) {
+                    row_result = {};
+                    row_result.zoo_id = rows[b].zoo_id;
+                    row_result.name = rows[b].name;
+                    row_result.postcode = rows[b].postcode;
+                    row_result.link = rows[b].link;
+                    row_result.species = values[b];
+                    data.push(row_result);
+                }
+                res.json(data);
+            });
+
         });
     } else {
         Zoos.getAllZoos().catch(function(err) {
