fdc2b4aa7201736f4bc91c6bd8fca87d85c72728
joshcoales <joshua@coales.co.uk>
2018-01-03 10:36:01 +0000

    Adding some example tests

---
+++ test/example.spec.js
@@ -0,0 +1,10 @@
+
+
+describe("An example test", function() {
+    it("Is a test?", function * () {
+        console.log("Example test")
+    })
+    it("Is still a test", function * () {
+        console.log("Blah~")
+    })
+})
