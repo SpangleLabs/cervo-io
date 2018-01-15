7bdb392b557836af3fdd7c7827751741a93c516d
joshcoales <joshua@coales.co.uk>
2018-01-15 17:39:56 +0000

    Adding chai-http, and testing that, to make life much easier

--- test/routes/categories.spec.js
+++ test/routes/categories.spec.js
@@ -1,12 +1,22 @@
-var RequestPromise = require("request-promise");
-var chai = require('chai');
-var expect = chai.expect;
-var chaiAsPromised = require("chai-as-promised");
+const RequestPromise = require("request-promise");
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
+
+const server = require("../../app");
 
 describe("Base category listing", function() {
     it("Should return a list", function * () {
         return expect(RequestPromise({"url":"http://localhost:3000/categories/", "json":true})).to.eventually.be.a("Array");
+    })
+    it("chai request works", function * () {
+        chai.request(server).get("/categories/").end(function(err, res) {
+            res.status.should.equal(200);
+            res.type.should.equal("application/json");
+        })
     })
 })
