8ba4a4107506d8cfe1ca0390bd760121ff970371
joshcoales <joshua@coales.co.uk>
2018-01-15 00:07:26 +0000

    Adding an actual test

---
+++ test/routes/categories.spec.js
@@ -0,0 +1,12 @@
+var RequestPromise = require("request-promise");
+var chai = require('chai');
+var expect = chai.expect;
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+
+describe("Base category listing", function() {
+    it("Should return a list", function * () {
+        expect(RequestPromise({"url":"http://localhost:3000/categories/", "json":true})).to.eventually.be.a("Array");
+    })
+})
