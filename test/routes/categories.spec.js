cabd7954d5bf744065220722a7354f076aa2ef10
joshcoales <joshua@coales.co.uk>
2018-01-15 23:22:33 +0000

    Implementing a full check of the base category response format

--- test/routes/categories.spec.js
+++ test/routes/categories.spec.js
@@ -1,8 +1,5 @@
-const RequestPromise = require("request-promise");
 const chai = require('chai');
 const expect = chai.expect;
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
@@ -10,13 +7,33 @@ chai.use(chaiHttp);
 const server = require("../../app");
 
 describe("Base category listing", function() {
-    it("Should return a list", function * () {
-        return expect(RequestPromise({"url":"http://localhost:3000/categories/", "json":true})).to.eventually.be.a("Array");
-    })
-    it("chai request works", function * () {
+    it("Format is correct", function (done) {
         chai.request(server).get("/categories/").end(function(err, res) {
-            res.status.should.equal(200);
-            res.type.should.equal("application/json");
+            expect(err).to.be.null;
+            expect(res.status).to.be.equal(200);
+            expect(res.type).to.be.equal("application/json");
+            expect(res).to.be.json;
+            expect(res.body).to.be.an("array");
+            expect(res.body.length).to.be.at.least(1);
+            for (let category of res.body) {
+                expect(category.category_id).to.be.a("number");
+                expect(category.name).to.be.a("string");
+                expect(category.category_level_id).to.be.a("number");
+                expect(category.parent_category_id).to.be.null;
+                expect(category.sub_categories).to.be.an("array");
+                expect(category.sub_categories.length).to.be.at.least(1);
+                for (let subCategory of category.sub_categories) {
+                    expect(subCategory.category_id).to.be.a("number");
+                    expect(subCategory.name).to.be.a("string");
+                    expect(subCategory.category_level_id).to.be.a("number");
+                    expect(subCategory.parent_category_id).to.be.a("number");
+                    expect(subCategory.parent_category_id).to.be.equal(category.category_id);
+                }
+                expect(category.species).to.be.an("array");
+                expect(category.species.length).to.be.equal(0);
+            }
+            expect(res.body)
+            done();
         })
     })
 })
