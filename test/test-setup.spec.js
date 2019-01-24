db311f8bf95a609548ed33fc1ba9642dee295e72
joshcoales <joshua@coales.co.uk>
2019-01-24 00:00:56 +0000

    Test setup to run the SQL create query

--- test/test-setup.spec.js
+++ test/test-setup.spec.js
@@ -1,9 +1,15 @@
 const sinon = require('sinon')
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
+const db = require("../dbconnection");
+const fs = require("fs");
 
 before(function () {
-    chai.use(sinonChai)
+    chai.use(sinonChai);
+    const setupSQL = fs.readFileSync('../sql/zoo_species.sql').toString();
+    db.connection.then(function(conn) {
+        return conn.query(setupSQL);
+    });
 })
 
 beforeEach(function () {
