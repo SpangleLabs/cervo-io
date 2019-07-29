1183545c3fd9649fdbf6fdc14e554bcb30e3aca7
joshcoales <joshua@coales.co.uk>
2019-07-29 23:10:06 +0100

    Adding a raw Map class, and using that rather than the awkward google async defer script tag

---
+++ src/Map.ts
@@ -0,0 +1,16 @@
+import config from "./config";
+import loadGoogleMapsApi from "load-google-maps-api";
+
+export class Map {
+
+    static loadGoogleMapsApi(): Promise<typeof google.maps> {
+        return loadGoogleMapsApi({key: config["google_maps_key"]});
+    }
+
+    static createMap(googleMaps: any, mapElement: HTMLElement): typeof googleMaps.Map {
+        return new googleMaps.Map(mapElement, {
+            center: {lat: 55, lng: -3},
+            zoom: 6
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import {SelectedSpecies} from "./selectedSpecies";
 import {SearchView} from "./searchView";
 import {PageMap} from "./pageMap";
 import {ViewSelector} from "./viewSelector";
+import {Map} from "./Map";
 
 let map: PageMap;
-let googleMap: google.maps.Map;
-const animalData: AnimalData = new AnimalData();
 let selector: ViewSelector;
 let selection: SelectedSpecies;
 
@@ -15,47 +14,54 @@ function userExpandCategory(id: number): void {
     const taxonomyView: TaxonomyView = <TaxonomyView>selector.views["taxonomical"];
     taxonomyView.categories[id].loadSubElements(true, false);
 }
+
 (<any>window).userExpandCategory = userExpandCategory;
 
 function userSelectCategory(categoryId: number): void {
     const taxonomyView: TaxonomyView = <TaxonomyView>selector.views["taxonomical"];
     const category = taxonomyView.categories[categoryId];
     category.select();
-    category.loadSubElements(false, true).then(function() {
+    category.loadSubElements(false, true).then(function () {
         //selection.update();
     });
 }
+
 (<any>window).userSelectCategory = userSelectCategory;
 
 function userSelectSpecies(speciesId: number): void {
     selection.toggleSpecies(speciesId);
 }
+
 (<any>window).userSelectSpecies = userSelectSpecies;
 
 function userUpdatePostcode(): void {
     selection.updateZooDistances();
 }
+
 (<any>window).userUpdatePostcode = userUpdatePostcode;
 
 function userSearchButton(): void {
     const searchView: SearchView = <SearchView>selector.views["search"];
     searchView.updateSearchResults();
 }
+
 (<any>window).userSearchButton = userSearchButton;
 
 function userToggleInfoWindow(zooId: number): void {
     map.toggleInfoWindow(zooId);
 }
+
 (<any>window).userToggleInfoWindow = userToggleInfoWindow;
 
-function initMap() {
-    googleMap = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
-        center: {lat: 55, lng: -3}
+document.addEventListener("DOMContentLoaded", function () {
+    let mapElement = document.getElementById('map');
+
+    Map.loadGoogleMapsApi().then(function (googleMaps: any) {
+        const googleMap = Map.createMap(googleMaps, mapElement);
+        map = new PageMap(googleMap);
+        const animalData: AnimalData = new AnimalData();
+        selection = new SelectedSpecies(animalData, map);
+        selector = new ViewSelector();
+        selector.initialise(animalData, selection);
     });
-    map = new PageMap(googleMap);
-    selection = new SelectedSpecies(animalData, map);
-    selector = new ViewSelector();
-    selector.initialise(animalData, selection);
-}
-(<any>window).initMap = initMap;
+});
