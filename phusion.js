7b63287a402817adfcb096230a52f2c4e38329d3
joshcoales <joshua@coales.co.uk>
2019-08-04 14:39:08 +0100

    Adding Phusion Passenger startup file, for hosting on hostpresto

---
+++ phusion.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var app = require('./index').app;
+
+http.createServer(app).listen(3000);
+if (typeof(PhusionPassenger) !== 'undefined') {
+    console.log('Example app running inside Passenger.');
+} else {
+    console.log('Example app listening on http://127.0.0.1:3000/');
+}
