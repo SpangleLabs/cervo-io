aa6b7bdd4475f95a8d10b5b6ebcd13117ffc7b8f
joshcoales <joshua@coales.co.uk>
2019-08-19 00:40:21 +0100

    Adding tests for category levels endpoints

---
+++ src/routes/categoryLevelsRouter.spec.ts
@@ -0,0 +1,64 @@
+import * as chai from 'chai';
+import {expect} from 'chai';
+import chaiHttp = require('chai-http');
+import {requestRouter, MockCategoryLevelsProvider} from "../testMocks";
+import {Number, String, Record} from "runtypes";
+import {CategoryLevelsRouter} from "./categoryLevelsRouter";
+
+chai.use(chaiHttp);
+
+const CategoryLevel = Record({
+    category_level_id: Number,
+    name: String
+});
+
+describe("All category levels listing", function() {
+    it("Format is correct", function (done) {
+        const mockCategoryLevelsProvider = new MockCategoryLevelsProvider([
+            {
+                category_level_id: 1, name: "Level 1"
+            },
+            {
+                category_level_id: 2, name: "Level 2"
+            }]);
+        const categoryLevelRouter = new CategoryLevelsRouter(mockCategoryLevelsProvider);
+
+        requestRouter(categoryLevelRouter).get("/category_levels/").end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res.status).to.be.equal(200);
+            expect(res.type).to.be.equal("application/json");
+            expect(res.body).to.be.an("array");
+            expect(res.body.length).to.be.equal(2);
+            for (let level of res.body) {
+                CategoryLevel.check(level);
+            }
+            done();
+        })
+    });
+});
+
+describe("View specific category level", function() {
+    it("Shows just one category", function(done) {
+        const mockCategoryLevelsProvider = new MockCategoryLevelsProvider([
+            {
+                category_level_id: 1, name: "Level 1"
+            },
+            {
+                category_level_id: 2, name: "Level 2"
+            }]);
+        const categoryLevelRouter = new CategoryLevelsRouter(mockCategoryLevelsProvider);
+
+        requestRouter(categoryLevelRouter).get("/category_levels/2").end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res.status).to.be.equal(200);
+            expect(res.type).to.be.equal("application/json");
+            expect(res.body).to.be.an("array");
+            expect(res.body.length).to.be.equal(1);
+            const level = res.body[0];
+            CategoryLevel.check(level);
+            expect(level.category_level_id).to.be.equal(2);
+            expect(level.name).to.be.equal("Level 2");
+            done();
+        })
+    })
+});
