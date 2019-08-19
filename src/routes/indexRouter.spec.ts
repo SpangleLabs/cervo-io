7550c584eddfdf12dac25ddbb5662cb1c1732be5
Joshua Coales <Joshua@Coales.co.uk>
2019-08-19 17:40:47 +0100

    Adding index router spec

---
+++ src/routes/indexRouter.spec.ts
@@ -0,0 +1,17 @@
+import {requestRouter} from "../testMocks";
+import {IndexRouter} from "./indexRouter";
+import {expect} from "chai";
+
+
+describe("Index endpoint", function() {
+    it('should return an empty object', function (done) {
+        const indexRouter = new IndexRouter();
+        requestRouter(indexRouter).end(function(err, res) {
+            expect(err).to.be.null;
+            expect(res.status).to.be.equal(200);
+            expect(res.type).to.be.equal("application/json");
+            expect(res.body).to.be.an("object");
+            expect(Object.keys(res.body).length).to.be.equal(0);
+        })
+    });
+});
