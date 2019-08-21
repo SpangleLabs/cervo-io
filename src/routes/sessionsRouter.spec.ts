c5253bf661b9ca04981017a33a4ce0a7de874fd5
Joshua Coales <Joshua@Coales.co.uk>
2019-08-21 12:47:51 +0100

    Starting sessionsRouter tests

---
+++ src/routes/sessionsRouter.spec.ts
@@ -0,0 +1,47 @@
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import {SessionsRouter} from "./sessionsRouter";
+import {Request} from "express";
+import {MockSessionsProvider} from "../testMocks";
+import {expect} from "chai";
+
+chai.use(chaiHttp);
+chai.use(require('chai-as-promised'));
+//const should = chai.should();
+
+describe("checkLogin() method", function() {
+    it('should raise error if auth token is not provided', function (done) {
+        const sessionsProvider = new MockSessionsProvider();
+        const sessionsRouter = new SessionsRouter(sessionsProvider);
+        const request = <Request><unknown>{
+            headers: {
+            },
+            connection: {
+                remoteAddress: "127.0.0.1"
+            }
+        };
+
+        sessionsRouter.checkLogin(request).then(function (token) {
+            done(new Error("Should not have returned token."));
+        }).catch(function (err) {
+            expect(err.toString()).to.be.equal("Error: No auth token provided.");
+            done();
+        })
+    });
+
+    it('should raise error if multiple session tokens are returned', function (done) {
+
+    });
+
+    it('should raise error if no session tokens are found', function (done) {
+
+    });
+
+    it('should return session token when valid auth token given', function (done) {
+
+    });
+
+    it('should get IP address from header, if available', function (done) {
+
+    });
+});
