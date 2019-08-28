f01fe2c01b8a393173a7b6f597fa563d53467ff4
joshcoales <joshua@coales.co.uk>
2019-08-28 21:59:40 +0100

    Moving some files around so that frontend-ui can be built by parcel

--- frontend-ui/src/config.ts
+++ frontend-ui/src/config.ts
@@ -1,6 +1,6 @@
 
 const config: {[key : string] : string} = {
-    "api_url": "https://ynnyxrq3yl.execute-api.eu-west-1.amazonaws.com/latest/",
+    "api_url": "https://api.cervo.io/",
     "google_maps_key": "AIzaSyA4CV7NT3SIw7vguuin6WkHnzxS9szP8do"
 };
 
