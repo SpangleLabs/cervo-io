bee25b3e20ff8b3fede1e9eb443f4066615da819
joshcoales <joshua@coales.co.uk>
2019-08-30 23:46:41 +0100

    Adding placeholder admin_config file, until all pages are using common-ui-lib correctly

--- admin-ui/src/admin_config.ts
+++ admin-ui/src/admin_config.ts
@@ -1,3 +1,7 @@
+
+// DELETE ME WHEN POSSIBLE
 export const config = {
-    "api_url": "http://api.cervo.io/"
+    "api_url": "http://api.cervo.io/",
+    "google_maps_key": "AIzaSyA4CV7NT3SIw7vguuin6WkHnzxS9szP8do"
 };
+
