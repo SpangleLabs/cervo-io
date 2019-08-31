ab178c70e4ba1885d30c5bbd73f99c3f3b0ee9d1
joshcoales <joshua@coales.co.uk>
2019-08-31 10:48:04 +0100

    Reorganising authCheck to throw error as error

--- admin-ui/src/index.ts
+++ admin-ui/src/index.ts
@@ -4,10 +4,8 @@ import {checkLogin, getAuthCookie} from "./lib/authCheck";
 $(document).ready(function() {
     const token = getAuthCookie();
     checkLogin(token).then(function(isLogin) {
-        if(isLogin) {
-            $("#login-status").text(`You are logged in as ${isLogin.username}`);
-        } else {
-            $("#login-status").html(`You are not logged in. <a href="login.html">Go to login</a>`);
-        }
+        $("#login-status").text(`You are logged in as ${isLogin.username}`);
+    }).catch(function (err) {
+        $("#login-status").html(`You are not logged in. <a href="login.html">Go to login</a>`);
     })
 });
--- admin-ui/src/lib/authCheck.ts
+++ admin-ui/src/lib/authCheck.ts
@@ -1,4 +1,3 @@
-import {config} from "../admin_config";
 import {promiseGet} from "@cervoio/common-ui-lib/src/utilities";
 import {SessionTokenJson} from "@cervoio/common-lib/src/apiInterfaces";
 
@@ -7,12 +6,12 @@ export function getAuthCookie(): string {
     return document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 }
 
-export function checkLogin(token: string): Promise<SessionTokenJson|null> {
+export function checkLogin(token: string): Promise<SessionTokenJson> {
     const headers = new Map<string, string>();
     headers.set("authorization", token);
-    return promiseGet(config['api_url'] + "session/", headers).then(function (response: SessionTokenJson) {
+    return promiseGet("session/", headers).then(function (response: SessionTokenJson) {
         return response;
     }).catch(function (err) {
-        return null;
+        throw err;
     });
 }
