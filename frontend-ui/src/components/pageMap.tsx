52a40c6773a552ce02fa808a7703df7334773e98
Joshua Coales <joshua@coales.co.uk>
2019-09-13 10:10:09 +0100

    Little more work on google maps info windows

--- frontend-ui/src/components/pageMap.tsx
+++ frontend-ui/src/components/pageMap.tsx
@@ -1,4 +1,4 @@
-import {Map, Marker} from 'google-maps-react';
+import {InfoWindow, Map as GoogleMap, Marker} from 'google-maps-react';
 import * as React from "react";
 //import config from "../config";
 import {ZooJson} from "../../../common-lib/src/apiInterfaces";
@@ -6,31 +6,36 @@ import {ZooJson} from "../../../common-lib/src/apiInterfaces";
 interface MapProps {
     google: any;
     selectedZoos: ZooJson[];
-    // visibleInfoWindows: Marker[];
+    selectedSpeciesIds: number[];
+    visibleInfoWindowsZooIds: number[];
 }
 
 export class MapContainer extends React.Component<MapProps, {}> {
     render() {
+        const currentMarkers: Map<number, JSX.Element> = new Map(this.props.selectedZoos.map((zoo) =>
+            [
+                zoo.zoo_id,
+                <Marker
+                    position={{lat: zoo.latitude, lng: zoo.longitude}}
+                    title={zoo.name}
+                />
+            ]));
+        const visibleInfoWindows = this.props.visibleInfoWindowsZooIds.map((x) =>
+            <InfoWindow
+                google={this.props.google}
+                map={map}
+                marker={currentMarkers.get(x)}
+            />
+        );
         return (
-            <Map
+            <GoogleMap
                 google={this.props.google}
                 zoom={6}
                 center={{lat: 55, lng: -3}}
             >
-                {
-                    this.props.selectedZoos.map((zoo) =>
-                        <Marker
-                            position={{lat: zoo.latitude, lng: zoo.longitude}}
-                            title={zoo.name}
-                        />)
-                }
-
-                {/*<InfoWindow onClose={this.onInfoWindowClose}>*/}
-                {/*    <div>*/}
-                {/*        <h1>{this.state.selectedPlace.name}</h1>*/}
-                {/*    </div>*/}
-                {/*</InfoWindow>*/}
-            </Map>
+                {currentMarkers.values()}
+                {visibleInfoWindows}
+            </GoogleMap>
         );
     }
 }
