2c988f20cac6eb880b88693d1a76d3e19dc373bf
joshcoales <joshua@coales.co.uk>
2019-09-21 22:06:24 +0100

    Renaming non selectable taxonomy for clarity

--- admin-ui/src/test/non_selectable_taxonomy.tsx
+++ admin-ui/src/test/non_selectable_taxonomy.tsx
@@ -1,9 +1,9 @@
 import * as ReactDOM from "react-dom";
 import {
     NonSelectableTaxonomyViewComponent
-} from "@cervoio/common-ui-lib/src/components/taxonomyView";
+} from "../common-ui-lib/src/components/taxonomyView";
 import * as React from "react";
-import {AnimalData} from "@cervoio/common-ui-lib/src/animalData";
+import {AnimalData} from "../common-ui-lib/src/animalData";
 
 document.addEventListener("DOMContentLoaded", async function() {
     const animalData = new AnimalData();
