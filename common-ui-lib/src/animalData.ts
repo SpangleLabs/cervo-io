4dd881f3c27123222873ebd928ea2396a3368a54
Joshua Coales <joshua@coales.co.uk>
2019-09-26 17:43:19 +0100

    Allowing auth token as a parameter for AnimalData class, using that for get requests

--- common-ui-lib/src/animalData.ts
+++ common-ui-lib/src/animalData.ts
@@ -10,9 +10,11 @@ import {
     SpeciesJson, ZooDistanceCache, ZooDistanceJson,
     ZooJson
 } from "@cervoio/common-lib/src/apiInterfaces";
+import {getAuthCookie} from "../../admin-ui/src/lib/authCheck";
 
 
 export class AnimalData {
+    token?: string;
     species: Map<number, SpeciesData>;
     categories: Map<number, CategoryData>;
     categoryLevels: Promise<CategoryLevelJson[]>;
@@ -22,16 +24,25 @@ export class AnimalData {
     cacheZooDistances: {[key: string]: {[key: string]: number}} = {};
     fullZoos: Map<number, FullZooJson>;
 
-    constructor() {
+    constructor(token?: string) {
+        this.token = token;
         this.species = new Map<number, SpeciesData>();
         this.categories = new Map<number, CategoryData>();
         this.speciesByLetter = new Map<string, Promise<SpeciesData[]>>();
         this.fullZoos = new Map();
     }
 
+    getPath(path: string): Promise<any> {
+        let authHeaders = undefined;
+        if(this.token) {
+            authHeaders = new Map([["authorization", getAuthCookie()]]);
+        }
+        return promiseGet(path, authHeaders);
+    }
+
     promiseCategoryLevels() : Promise<CategoryLevelJson[]> {
         if (!this.categoryLevels) {
-            this.categoryLevels = promiseGet("category_levels/");
+            this.categoryLevels = this.getPath("category_levels/");
         }
         return this.categoryLevels;
     }
@@ -39,7 +50,7 @@ export class AnimalData {
     promiseBaseCategories() : Promise<CategoryData[]> {
         if (!this.baseCategory) {
             const self = this;
-            this.baseCategory = promiseGet("categories/").then(function(data: CategoryJson[]) {
+            this.baseCategory = this.getPath("categories/").then(function(data: CategoryJson[]) {
                 return data.map(x => self.getOrCreateCategory(x));
             });
         }
@@ -48,7 +59,7 @@ export class AnimalData {
 
     promiseValidFirstLetters() : Promise<string[]> {
         if (!this.validFirstLetters) {
-            this.validFirstLetters = promiseGet("species/valid_first_letters/");
+            this.validFirstLetters = this.getPath("species/valid_first_letters/");
         }
         return this.validFirstLetters;
     }
@@ -56,7 +67,7 @@ export class AnimalData {
     promiseSpeciesByLetter(letter: string) : Promise<SpeciesData[]> {
         if (!this.speciesByLetter.has(letter)) {
             const self = this;
-            const speciesPromise = promiseGet(`species/?common_name=${letter}%25`).then(function(data: SpeciesJson[]) {
+            const speciesPromise = this.getPath(`species/?common_name=${letter}%25`).then(function(data: SpeciesJson[]) {
                 return data.map(x => self.getOrCreateSpecies(x));
             });
             this.speciesByLetter.set(letter, speciesPromise);
@@ -66,7 +77,7 @@ export class AnimalData {
 
     promiseSearchSpecies(search: string) : Promise<SpeciesData[]> {
         const self = this;
-        return promiseGet(`species/?name=%25${search}%25`).then(function(animals: SpeciesJson[]) {
+        return this.getPath(`species/?name=%25${search}%25`).then(function(animals: SpeciesJson[]) {
             return animals.map(x => self.getOrCreateSpecies(x));
         });
     }
@@ -85,7 +96,7 @@ export class AnimalData {
         if (this.species.has(speciesId)) {
             return this.species.get(speciesId);
         } else {
-            const newSpecies = new SpeciesData(speciesData);
+            const newSpecies = new SpeciesData(speciesData, this);
             this.species.set(speciesId, newSpecies);
             return newSpecies;
         }
@@ -122,7 +133,7 @@ export class AnimalData {
         //create url to request
         let path = `zoo_distances/${postcode}/`+zoosNeedingDistance.join(",");
         //get response
-        const newDistances = await promiseGet(path);
+        const newDistances = await this.getPath(path);
         this.cacheAddZooDistances(postcode, newDistances);
         return foundDistances.concat(newDistances);
     }
@@ -131,7 +142,7 @@ export class AnimalData {
         if(this.fullZoos.get(zooId)) {
             return this.fullZoos.get(zooId);
         }
-        const fullData = await promiseGet(`zoos/${zooId}`);
+        const fullData = await this.getPath(`zoos/${zooId}`);
         this.fullZoos.set(zooId, fullData[0]);
         return fullData[0];
     }
@@ -160,7 +171,7 @@ export class CategoryData {
 
     async promiseFullData(): Promise<FullCategoryJson> {
         if (!this._fullDataPromise) {
-            this._fullDataPromise = promiseGet("categories/" + this.id).then(function (data: FullCategoryJson[]) {
+            this._fullDataPromise = this.animalData.getPath("categories/" + this.id).then(function (data: FullCategoryJson[]) {
                 return data[0];
             });
         }
@@ -196,13 +207,15 @@ export class CategoryData {
  * Stores species info, and lists of zoos the species appear in
  */
 export class SpeciesData {
+    animalData: AnimalData;
     id: number;
     commonName: string;
     latinName: string;
     parentCategoryId: number;
     zooList: Promise<ZooJson[]> | null;
 
-    constructor(speciesData: SpeciesJson) {
+    constructor(speciesData: SpeciesJson, animalData: AnimalData) {
+        this.animalData = animalData;
         this.id = speciesData.species_id;
         this.commonName = speciesData.common_name;
         this.latinName = speciesData.latin_name;
@@ -212,7 +225,7 @@ export class SpeciesData {
 
     async getZooList(): Promise<ZooJson[]> {
         if(this.zooList == null) {
-            this.zooList = promiseGet("species/" + this.id).then(function (data: FullSpeciesJson[]) {
+            this.zooList = this.animalData.getPath("species/" + this.id).then(function (data: FullSpeciesJson[]) {
                 return data[0].zoos;
             });
         }
