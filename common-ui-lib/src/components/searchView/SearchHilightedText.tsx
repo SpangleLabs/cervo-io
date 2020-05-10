d1f7ff0284865044dd4c84670f0469738516ca88
Joshua Coales <joshua@coales.co.uk>
2020-05-11 00:37:40 +0100

    Converting SearchHilightedText component to functional form

--- common-ui-lib/src/components/searchView/SearchHilightedText.tsx
+++ common-ui-lib/src/components/searchView/SearchHilightedText.tsx
@@ -6,19 +6,18 @@ interface HilightedTextProps {
     text: string;
     searchTerm: string;
 }
-export class SearchHilightedText extends React.Component<HilightedTextProps, {}> {
-    render() {
-        const searchRegex = new RegExp(this.props.searchTerm, "gi");
-        this.props.text.split(searchRegex);
 
-        let split = this.props.text.split(searchRegex);
-        let replacements = this.props.text.match(searchRegex);
-        let result = [];
-        for (let i = 0; i < split.length - 1; i++) {
-            result.push(split[i]);
-            result.push(<span className={style.searchTerm}>{replacements[i]}</span>);
-        }
-        result.push(split[split.length - 1]);
-        return result;
+export const SearchHilightedText: React.FunctionComponent<HilightedTextProps> = (props) => {
+    const searchRegex = new RegExp(props.searchTerm, "gi");
+    props.text.split(searchRegex);
+
+    let split = props.text.split(searchRegex);
+    let replacements = props.text.match(searchRegex);
+    let result = [];
+    for (let i = 0; i < split.length - 1; i++) {
+        result.push(split[i]);
+        result.push(<span className={style.searchTerm}>{replacements[i]}</span>);
     }
+    result.push(split[split.length - 1]);
+    return <>result</>;
 }
