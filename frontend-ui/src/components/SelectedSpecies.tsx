6e7b200938bc7e9e278b32e5af15fef288e73b0d
Joshua Coales <joshua@coales.co.uk>
2020-05-11 22:33:05 +0100

    Swapping SelectedSpecies to functional component, neatening up a bit

--- frontend-ui/src/components/SelectedSpecies.tsx
+++ frontend-ui/src/components/SelectedSpecies.tsx
@@ -5,6 +5,7 @@ import {ZooJson} from "@cervoio/common-lib/src/apiInterfaces";
 import {SelectedSpeciesResult} from "./SelectedSpeciesResult";
 import {PostcodeEntry} from "./PostcodeEntry";
 import {SelectedZooResult} from "./SelectedZooResult";
+import {SpeciesData} from "@cervoio/common-ui-lib/src/animalData";
 
 interface SelectedSpeciesComponentProps extends ViewProps {
     selectedZoos: ZooJson[];
@@ -17,44 +18,41 @@ interface SelectedSpeciesComponentProps extends ViewProps {
     loadingZoos: boolean;
 }
 
-export class SelectedSpeciesComponent extends React.Component<SelectedSpeciesComponentProps, {}> {
-    constructor(props: SelectedSpeciesComponentProps) {
-        super(props);
-    }
-
-    render() {
-        const selectedSpecies = this.props.selectedSpeciesIds.map((speciesId) => {return this.props.animalData.species.get(speciesId)});
-        selectedSpecies.sort((a, b) => a.commonName.localeCompare(b.commonName));
-        return <><h2>Selected species ({this.props.selectedSpeciesIds.length})</h2>
-            <ul>
-                {selectedSpecies.map((species) =>
-                    <SelectedSpeciesResult
-                        key={species.id}
-                        species={species}
-                        onSelectSpecies={this.props.onSelectSpecies}
-                    />)
-                }
-            </ul>
-            <PostcodeEntry
-                postcode={this.props.postcode}
-                error={this.props.postcodeError}
-                onUpdate={this.props.onPostcodeUpdate}
-                isLoading={this.props.loadingDistances}
-            />
-            <h2>Zoos with selected species ({this.props.selectedZoos.length})</h2>
-            {this.props.loadingDistances ? <Spinner /> : ""}
-            <ul id="selected-zoos">
-                {this.props.selectedZoos.map((zoo) =>
-                {
-                    const onSelect = this.props.onSelectZoos.bind(null, zoo);
-                    return <SelectedZooResult
-                        key={zoo.zoo_id}
-                        zoo={zoo}
-                        onSelect={onSelect}
-                        distance={this.props.zooDistances.get(zoo.zoo_id)}
-                    />
-                })}
-            </ul>
-        </>
-    }
+export const SelectedSpeciesComponent: React.FunctionComponent<SelectedSpeciesComponentProps> = (props) => {
+    const selectedSpecies = props.selectedSpeciesIds.map((speciesId) => {
+        return props.animalData.species.get(speciesId)
+    }).filter((x): x is SpeciesData => x !== undefined);
+    selectedSpecies.sort(
+        (a, b) => a.commonName.localeCompare(b.commonName)
+    );
+    return <><h2>Selected species ({props.selectedSpeciesIds.length})</h2>
+        <ul>
+            {selectedSpecies.map((species) =>
+                <SelectedSpeciesResult
+                    key={species.id}
+                    species={species}
+                    onSelectSpecies={props.onSelectSpecies}
+                />)
+            }
+        </ul>
+        <PostcodeEntry
+            postcode={props.postcode}
+            error={props.postcodeError}
+            onUpdate={props.onPostcodeUpdate}
+            isLoading={props.loadingDistances}
+        />
+        <h2>Zoos with selected species ({props.selectedZoos.length})</h2>
+        {props.loadingDistances ? <Spinner/> : ""}
+        <ul id="selected-zoos">
+            {props.selectedZoos.map((zoo) => {
+                const onSelect = props.onSelectZoos.bind(null, zoo);
+                return <SelectedZooResult
+                    key={zoo.zoo_id}
+                    zoo={zoo}
+                    onSelect={onSelect}
+                    distance={props.zooDistances.get(zoo.zoo_id)}
+                />
+            })}
+        </ul>
+    </>
 }
