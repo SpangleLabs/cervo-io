38104fdcb3d043f84d8c44849e27766606d365b5
Joshua Coales <joshua@coales.co.uk>
2020-05-13 00:38:13 +0100

    Ordering zoos in the SelectedZoosList component

--- frontend-ui/src/components/SelectedZoosList.tsx
+++ frontend-ui/src/components/SelectedZoosList.tsx
@@ -11,11 +11,29 @@ interface SelectedZoosListProps {
 }
 
 export const SelectedZoosList: React.FunctionComponent<SelectedZoosListProps> = (props) => {
+
+    // handle ordering of zoos here.
+    const cmp:(a: boolean | number | undefined, b: boolean | number | undefined) => number = (a, b) => {
+        if (a === undefined && b === undefined) return 0;
+        if (a === undefined) return +1;
+        if (b === undefined) return -1;
+        if (a > b) return +1;
+        if (a < b) return -1;
+        return 0;
+    }
+    const compareZoos = (a: ZooJson, b: ZooJson) => {
+        return cmp(!props.zooDistances.has(a.zoo_id), !props.zooDistances.has(b.zoo_id))
+            || cmp(props.zooDistances.get(a.zoo_id), props.zooDistances.get(b.zoo_id))
+            || a.name.localeCompare(b.name)
+    }
+
+    const orderedZoos = props.selectedZoos.sort(compareZoos);
+
     return <>
     <h2>Zoos with selected species ({props.selectedZoos.length})</h2>
         {props.loadingDistances ? <Spinner/> : ""}
         <ul id="selected-zoos">
-            {props.selectedZoos.map((zoo) => {
+            {orderedZoos.map((zoo) => {
                 const onSelect = props.onSelectZoos.bind(null, zoo);
                 return <SelectedZooResult
                     key={zoo.zoo_id}
