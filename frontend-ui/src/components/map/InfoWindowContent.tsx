d7af18ec0c8826b638c5879fde4fce091e17b8d2
Joshua Coales <joshua@coales.co.uk>
2020-05-13 01:14:11 +0100

    Converting InfoWindowContent to functional component including css-modules

--- frontend-ui/src/components/map/InfoWindowContent.tsx
+++ frontend-ui/src/components/map/InfoWindowContent.tsx
@@ -1,28 +1,38 @@
 import * as React from "react";
 import {FullZooJson} from "@cervoio/common-lib/src/apiInterfaces";
 import {LatinName} from "@cervoio/common-ui-lib/src/components/LatinName";
+import classNames from "classnames";
+
+const styles = require("./InfoWindowContent.css")
 
 interface InfoWindowContentProps {
     zoo: FullZooJson;
     selectedSpeciesIds: number[];
 }
-export class InfoWindowContent extends React.Component<InfoWindowContentProps, {}> {
-    render() {
-        return <>
-            <h1>{this.props.zoo.name}</h1>
-            <a href={this.props.zoo.link}>{this.props.zoo.link}</a><br/>
-            <span>Postcode: </span>{this.props.zoo.postcode}<br/>
-            <h2>Species:</h2>
-            <ul className="zoo_species">
-                {this.props.zoo.species.map((species) =>
-                    <li key={species.species_id}>
-                        <span className={`zoo_species species ${this.props.selectedSpeciesIds.includes(species.species_id) ? "selected" : ""}`}>
-                            <span className="common_name">{species.common_name}</span>
+
+export const InfoWindowContent: React.FunctionComponent<InfoWindowContentProps> = (props) => {
+    return <>
+        <h1>{props.zoo.name}</h1>
+        <a href={props.zoo.link}>{props.zoo.link}</a><br/>
+        <span>Postcode: </span>{props.zoo.postcode}<br/>
+        <h2>Species:</h2>
+        <ul className={styles.zoo_species}>
+            {props.zoo.species.map((species) => {
+                    const classes = classNames(
+                        styles.zoo_species,
+                        styles.species,
+                        {
+                            [styles.selected]: props.selectedSpeciesIds.includes(species.species_id)
+                        }
+                    )
+                    return <li key={species.species_id}>
+                        <span className={classes}>
+                            <span>{species.common_name}</span>
                             <LatinName>{species.latin_name}</LatinName>
                         </span>
                     </li>
-                )}
-            </ul>
-        </>
-    }
+                }
+            )}
+        </ul>
+    </>
 }
