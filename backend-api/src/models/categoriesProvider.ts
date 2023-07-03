e3e771d8a53ebba2eebf77b4bf4ef3618a8c4889
JC <joshua@coales.co.uk>
2023-07-04 00:30:09 +0100

    Convert categories provider to async and postgres

--- backend-api/src/models/categoriesProvider.ts
+++ backend-api/src/models/categoriesProvider.ts
@@ -2,15 +2,15 @@ import {AbstractProvider} from "./abstractProvider"
 import {CategoryJson, NewCategoryJson} from "@cervoio/common-lib/src/apiInterfaces"
 
 function processIntoCategoryJson(data: CategoryJson[] | any): CategoryJson[] {
-    return data.map(function (datum: CategoryJson | any) {
-        return {
+    return data.map((datum: CategoryJson | any) => (
+        {
             category_id: datum.category_id,
             name: datum.name,
             category_level_id: datum.category_level_id,
             parent_category_id: datum.parent_category_id,
-            hidden: datum.hidden
+            hidden: datum.hidden,
         }
-    });
+    ))
 }
 
 export class CategoriesProvider extends AbstractProvider {
@@ -22,54 +22,54 @@ export class CategoriesProvider extends AbstractProvider {
         return processIntoCategoryJson(result.rows)
     }
 
-    getCategoryById(id: number): Promise<CategoryJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from categories where category_id=?", [id]);
-            conn.end();
-            return result;
-        }).then(processIntoCategoryJson);
+    async getCategoryById(id: number): Promise<CategoryJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from categories where category_id=$1",
+            [id]
+        )
+        await this.client.end()
+        return processIntoCategoryJson(result.rows)
     }
 
-    getCategoriesByParentId(id: number): Promise<CategoryJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from categories where parent_category_id=? order by `name`", [id]);
-            conn.end();
-            return result;
-        }).then(processIntoCategoryJson);
+    async getCategoriesByParentId(id: number): Promise<CategoryJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from categories where parent_category_id=$1 order by name",
+            [id]
+        )
+        await this.client.end()
+        return processIntoCategoryJson(result.rows)
     }
 
-    addCategory(newCategory: NewCategoryJson): Promise<CategoryJson> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("insert into categories (`name`,`category_level_id`,`parent_category_id`) " +
-                "values (?,?,?)", [newCategory.name, newCategory.category_level_id, newCategory.parent_category_id]);
-            conn.end();
-            return result.then(function (data: NewEntryData) {
-                const result: CategoryJson = {
-                    category_id: data.insertId,
-                    category_level_id: newCategory.category_level_id,
-                    name: newCategory.name,
-                    parent_category_id: newCategory.parent_category_id,
-                    hidden: newCategory.hidden
-                };
-                return result;
-            })
-        });
+    async addCategory(newCategory: NewCategoryJson): Promise<CategoryJson> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "insert into categories (`name`,`category_level_id`,`parent_category_id`) values ($1,$2,$3) returning category_id",
+            [newCategory.name, newCategory.category_level_id, newCategory.parent_category_id]
+        )
+        await this.client.end()
+        return {
+            category_id: result.rows[0].category_id,
+            category_level_id: newCategory.category_level_id,
+            name: newCategory.name,
+            parent_category_id: newCategory.parent_category_id,
+            hidden: newCategory.hidden,
+        }
     }
 
-    deleteCategory(id: number): Promise<void> {
-        return this.connection().then(function (conn) {
-            conn.query("delete from categories where zoo_id=?", [id]);
-            conn.end();
-            return;
-        });
+    async deleteCategory(id: number): Promise<void> {
+        await this.client.connect()
+        await this.client.query("delete from categories where zoo_id=$1", [id])
+        await this.client.end()
     }
 
-    updateCategory(id: number, updatedCategory: NewCategoryJson): Promise<void> {
-        return this.connection().then(function (conn) {
-            conn.query("update categories set name=?, category_level_id=?, parent_category_id=? where category_id=?",
-                [updatedCategory.name, updatedCategory.category_level_id, updatedCategory.parent_category_id, id]);
-            conn.end();
-            return;
-        });
+    async updateCategory(id: number, updatedCategory: NewCategoryJson): Promise<void> {
+        await this.client.connect()
+        await this.client.query(
+            "update categories set name=$1, category_level_id=$2, parent_category_id=$3 where category_id=$4",
+            [updatedCategory.name, updatedCategory.category_level_id, updatedCategory.parent_category_id, id]
+        )
+        await this.client.end()
     }
 }
