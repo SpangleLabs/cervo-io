7bed81bbd418097f6c80463e73813ddc98345468
JC <joshua@coales.co.uk>
2023-07-04 07:54:38 +0100

    Convert zoo species provider to postgres and async

--- backend-api/src/models/zooSpeciesProvider.ts
+++ backend-api/src/models/zooSpeciesProvider.ts
@@ -1,66 +1,67 @@
 import {AbstractProvider} from "./abstractProvider";
 import {NewZooSpeciesLinkJson, ZooSpeciesLinkJson} from "@cervoio/common-lib/src/apiInterfaces";
-import {NewEntryData} from "../dbInterfaces";
 
 function hasUniqueId(arg: any): arg is ZooSpeciesLinkJson {
     return arg.zoo_species_id !== undefined;
 }
 
 function processIntoZooSpeciesLinkJson(data: ZooSpeciesLinkJson[] | any): ZooSpeciesLinkJson[] {
-    return data.map(function (datum: ZooSpeciesLinkJson | any) {
-        return {
-            zoo_species_id: datum.zoo_species_id,
-            zoo_id: datum.zoo_id,
-            species_id: datum.species_id
-        }
-    });
+    return data.map((datum: ZooSpeciesLinkJson | any)  => ({
+        zoo_species_id: datum.zoo_species_id,
+        zoo_id: datum.zoo_id,
+        species_id: datum.species_id,
+    }));
 }
 
 export class ZooSpeciesProvider extends AbstractProvider {
 
-    getZooSpeciesByZooId(id: number): Promise<ZooSpeciesLinkJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from zoo_species where zoo_id=?", [id]);
-            conn.end();
-            return result;
-        }).then(processIntoZooSpeciesLinkJson);
+    async getZooSpeciesByZooId(id: number): Promise<ZooSpeciesLinkJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from zoo_species where zoo_id=$1",
+            [id]
+        )
+        await this.client.end()
+        return processIntoZooSpeciesLinkJson(result.rows)
     }
 
-    getZooSpeciesBySpeciesId(id: number): Promise<ZooSpeciesLinkJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from zoo_species where species_id=?", [id]);
-            conn.end();
-            return result;
-        }).then(processIntoZooSpeciesLinkJson);
+    async getZooSpeciesBySpeciesId(id: number): Promise<ZooSpeciesLinkJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from zoo_species where species_id=$1",
+            [id]
+        )
+        await this.client.end()
+        return processIntoZooSpeciesLinkJson(result.rows)
     }
 
-    addZooSpecies(newZooSpecies: NewZooSpeciesLinkJson): Promise<ZooSpeciesLinkJson> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("insert into zoo_species (`zoo_id`,`species_id`) " +
-                "values (?,?)", [newZooSpecies.zoo_id, newZooSpecies.species_id]);
-            conn.end();
-            return result.then(function (data: NewEntryData) {
-                const result: ZooSpeciesLinkJson = {
-                    zoo_species_id: data.insertId,
-                    zoo_id: newZooSpecies.zoo_id,
-                    species_id: newZooSpecies.species_id
-                };
-                return result;
-            })
-        });
+    async addZooSpecies(newZooSpecies: NewZooSpeciesLinkJson): Promise<ZooSpeciesLinkJson> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "insert into zoo_species (`zoo_id`,`species_id`) values ($1,$2) returning zoo_species_id",
+            [newZooSpecies.zoo_id, newZooSpecies.species_id],
+        )
+        await this.client.end()
+        return {
+            zoo_species_id: result.rows[0].zoo_species_id,
+            zoo_id: newZooSpecies.zoo_id,
+            species_id: newZooSpecies.species_id,
+        }
     }
 
-    deleteZooSpecies(deleteLink: ZooSpeciesLinkJson | NewZooSpeciesLinkJson): Promise<void> {
-        return this.connection().then(function (conn) {
-            if (hasUniqueId(deleteLink)) {
-                conn.query("delete from zoo_species where zoo_species_id=?", [deleteLink.zoo_species_id]);
-                conn.end();
-                return;
-            } else {
-                conn.query("delete from zoo_species where zoo_id=? and species_id=?", [deleteLink.zoo_id, deleteLink.species_id]);
-                conn.end();
-                return;
-            }
-        });
+    async deleteZooSpecies(deleteLink: ZooSpeciesLinkJson | NewZooSpeciesLinkJson): Promise<void> {
+        await this.client.connect()
+        if (hasUniqueId(deleteLink)) {
+            await this.client.query(
+                "delete from zoo_species where zoo_species_id=$1",
+                [deleteLink.zoo_species_id]
+            )
+        } else {
+            await this.client.query(
+                "delete from zoo_species where zoo_id=$1 and species_id=$2",
+                [deleteLink.zoo_id, deleteLink.species_id]
+            )
+        }
+        await this.client.end()
     }
 }
