4d0eaca4b73b65693ab321f9b498e17843a859da
JC <joshua@coales.co.uk>
2023-07-04 07:46:30 +0100

    Convert user postcodes provider to postgres and async

--- backend-api/src/models/userPostcodesProvider.ts
+++ backend-api/src/models/userPostcodesProvider.ts
@@ -1,54 +1,54 @@
-import {AbstractProvider} from "./abstractProvider";
-import {NewUserPostcodeJson, UserPostcodeJson} from "@cervoio/common-lib/src/apiInterfaces";
-import {NewEntryData} from "../dbInterfaces";
+import {AbstractProvider} from "./abstractProvider"
+import {NewUserPostcodeJson, UserPostcodeJson} from "@cervoio/common-lib/src/apiInterfaces"
 
 function processIntoUserPostcodeJson(data: UserPostcodeJson[] | any): UserPostcodeJson[] {
-    return data.map(function (datum: UserPostcodeJson | any) {
-        return {
-            user_postcode_id: datum.user_postcode_id,
-            postcode_sector: datum.postcode_sector
-        }
-    });
+    return data.map((datum: UserPostcodeJson | any) => ({
+        user_postcode_id: datum.user_postcode_id,
+        postcode_sector: datum.postcode_sector,
+    }))
 }
 
 export class UserPostcodesProvider extends AbstractProvider {
 
-    getUserPostcodeById(id: number): Promise<UserPostcodeJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from user_postcodes where user_postcode_id=?", [id]);
-            conn.end();
-            return result;
-        }).then(processIntoUserPostcodeJson);
+    async getUserPostcodeById(id: number): Promise<UserPostcodeJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from user_postcodes where user_postcode_id=$1",
+            [id],
+        )
+        await this.client.end()
+        return processIntoUserPostcodeJson(result.rows)
     }
 
-    getUserPostcodeByPostcodeSector(sector: string): Promise<UserPostcodeJson[]> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("select * from user_postcodes where postcode_sector=?", [sector]);
-            conn.end();
-            return result;
-        }).then(processIntoUserPostcodeJson);
+    async getUserPostcodeByPostcodeSector(sector: string): Promise<UserPostcodeJson[]> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "select * from user_postcodes where postcode_sector=$1",
+            [sector]
+        )
+        await this.client.end()
+        return processIntoUserPostcodeJson(result)
     }
 
-    addUserPostcode(newUserPostcode: NewUserPostcodeJson): Promise<UserPostcodeJson> {
-        return this.connection().then(function (conn) {
-            const result = conn.query("insert into user_postcodes (`postcode_sector`) " +
-                "values (?)", [newUserPostcode.postcode_sector]);
-            conn.end();
-            return result.then(function (data: NewEntryData) {
-                const newRow: UserPostcodeJson = {
-                    user_postcode_id: data.insertId,
-                    postcode_sector: newUserPostcode.postcode_sector
-                };
-                return newRow;
-            });
-        });
+    async addUserPostcode(newUserPostcode: NewUserPostcodeJson): Promise<UserPostcodeJson> {
+        await this.client.connect()
+        const result = await this.client.query(
+            "insert into user_postcodes (`postcode_sector`) values ($1) returning user_postcode_id",
+            [newUserPostcode.postcode_sector],
+        )
+        await this.client.end()
+        return {
+            user_postcode_id: result.rows[0].user_postcode_id,
+            postcode_sector: newUserPostcode.postcode_sector,
+        }
     }
 
-    deleteUserPostcode(id: string): Promise<void> {
-        return this.connection().then(function (conn) {
-            conn.query("delete from user_postcodes where user_postcodes_id=?", [id]);
-            conn.end();
-            return;
-        });
+    async deleteUserPostcode(id: string): Promise<void> {
+        await this.client.connect()
+        await this.client.query(
+            "delete from user_postcodes where user_postcodes_id=$1",
+            [id],
+        )
+        await this.client.end()
     }
 }
